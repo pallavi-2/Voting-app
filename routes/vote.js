c9a3b0fe0fca3f2a468316c510450930d5029b42
Pallavi-2 <pallavipandey299@gmail.com>
2023-02-24 20:19:58 +0545

    Retrieve changes

--- routes/vote.js
+++ routes/vote.js
@@ -24,7 +24,7 @@ router.post('/createpoll', async (req, res) => {
 router.get('/createdpolls',async(req,res)=>{
     try{
         const userId = req.user.userId
-        const createdPolls = await Poll.find({userId})
+        const createdPolls = await Poll.find({createdBy:userId})
         res.send(createdPolls)
     }catch(err){
         res.status(400).json(err)
